7704cad357845de7b7f82c6f29472145345737ca
usha-12 <usha12ahirwar@gmail.com>
2023-01-20 18:06:07 +0530

    Added: Welcome to the Maximum Elements program

--- src/com/brideglabz/findmaximumproblemusinggenerics/MaximumElements.java
+++ src/com/brideglabz/findmaximumproblemusinggenerics/MaximumElements.java
@@ -1,2 +1,7 @@
-package com.brideglabz.findmaximumproblemusinggenerics;public class MaximumElements {
+package com.brideglabz.findmaximumproblemusinggenerics;
+
+public class MaximumElements {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Find Maximum Problem using Generics");
+    }
 }
